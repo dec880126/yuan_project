6cefbe4fd868a37490b31703cd580a083f128251
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-06-18 00:21:01 +0800

    Update yuan_b1.py

--- yuan_b1.py
+++ yuan_b1.py
@@ -1,11 +1,16 @@
 class SimulateSaving():
     def __init__(self, totalCost = 1000000, portionDownPayment = 0.25, interestRate = 0.04, annualSalary = 120000) -> None:
+        self.totalCost = totalCost
+        self.portionDownPayment = portionDownPayment
         self.fisrtCost = float(totalCost) * float(portionDownPayment)
         self.interestRate = float(interestRate)
         self.annualSalary = float(annualSalary)
         self.currentSavings = float(0)
         self.monthlySalary = float(self.annualSalary/12)
     
+    def __str__(self):
+        return f"{self.totalCost}, {self.portionDownPayment}, {self.interestRate}, {self.annualSalary}"
+        
     def simulate(self, portionSavings = 0.1) -> int:
         monthToSave = 0
         while self.currentSavings < self.fisrtCost:
@@ -19,5 +24,6 @@ annualSalary_ = input("annualSalary:")
 portionSavings_ = float(input("portionSavings:"))
 interestRate_ = input("interestRate:")
 
-me = SimulateSaving(totalCost = totalCost_, interestRate = interestRate_, annualSalary = annualSalary_)
-print(me.simulate(portionSavings_))
+# me = SimulateSaving(totalCost = totalCost_, interestRate = interestRate_, annualSalary = annualSalary_)
+# print(me.simulate(portionSavings_))
+# print(me)
