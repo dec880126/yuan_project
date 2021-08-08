7fcb0b1a4dbacbfa79415e2aa110a6903def87b9
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-08-08 10:10:40 +0800

    Update yuan_b1.py

--- yuan_b1.py
+++ yuan_b1.py
@@ -1,23 +1,30 @@
-class SimulateSaving():
-    def __init__(self, totalCost = 1000000, portionDownPayment = 0.25, interestRate = 0.04, annualSalary = 120000) -> None:
+class SimulateSaving:
+    def __init__(
+        self,
+        totalCost=1000000,
+        portionDownPayment=0.25,
+        interestRate=0.04,
+        annualSalary=120000,
+    ) -> None:
         self.totalCost = totalCost
         self.portionDownPayment = portionDownPayment
         self.fisrtCost = float(totalCost) * float(portionDownPayment)
         self.interestRate = float(interestRate)
         self.annualSalary = float(annualSalary)
         self.currentSavings = float(0)
-        self.monthlySalary = float(self.annualSalary/12)
-    
+        self.monthlySalary = float(self.annualSalary / 12)
+
     def __str__(self):
         return f"{self.totalCost}, {self.portionDownPayment}, {self.interestRate}, {self.annualSalary}"
-        
-    def simulate(self, portionSavings = 0.1) -> int:
+
+    def simulate(self, portionSavings=0.1) -> int:
         monthToSave = 0
         while self.currentSavings < self.fisrtCost:
-            self.currentSavings *= 1 + self.interestRate/12
-            self.currentSavings += self.monthlySalary * portionSavings            
+            self.currentSavings *= 1 + self.interestRate / 12
+            self.currentSavings += self.monthlySalary * portionSavings
             monthToSave += 1
         return monthToSave
+
 
 totalCost_ = input("totalCost:")
 annualSalary_ = input("annualSalary:")
