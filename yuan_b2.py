df36a7100cec62a8f9b3c2708031eeab24e39854
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-08-08 10:11:09 +0800

    Update yuan_b2.py

--- yuan_b2.py
+++ yuan_b2.py
@@ -1,11 +1,17 @@
-class SimulateSaving():
-    def __init__(self, totalCost = 1000000, portionDownPayment = 0.25, interestRate = 0.04, annualSalary = 120000):
+class SimulateSaving:
+    def __init__(
+        self,
+        totalCost=1000000,
+        portionDownPayment=0.25,
+        interestRate=0.04,
+        annualSalary=120000,
+    ):
         """
         type totalCost: int
         type portionDownPayment: float
         type interestRate: float
         type annualSalary: int
-        """   
+        """
         self.totalCost = totalCost
         self.portionDownPayment = portionDownPayment
         self.totalCost = int(totalCost)
@@ -15,48 +21,56 @@ class SimulateSaving():
 
     def __str__(self):
         return f"{self.totalCost}, {self.portionDownPayment}, {self.interestRate}, {self.annualSalary}"
-    
-    def simulate(self, portionSavings = 0.1) -> int:
+
+    def simulate(self, portionSavings=0.1) -> int:
         """
         type portionSavings: float
         rtype: int
         """
         monthToSave = 0
         currentSavings = 0.0
-        monthlySalary = self.annualSalary/12
+        monthlySalary = self.annualSalary / 12
         fisrtCost = self.totalCost * self.portionDownPayment
         while currentSavings < fisrtCost:
-            currentSavings *= 1 + self.interestRate/12              # interest of the money
-            currentSavings += monthlySalary * portionSavings        # save money by parameter: portionSavings
+            currentSavings *= 1 + self.interestRate / 12  # interest of the money
+            currentSavings += (
+                monthlySalary * portionSavings
+            )  # save money by parameter: portionSavings
             monthToSave += 1
         return monthToSave
 
-class SearchSavingsRate(SimulateSaving):        
-    def binarySearch(self, monthsToSave = 36):
+
+class SearchSavingsRate(SimulateSaving):
+    def binarySearch(self, monthsToSave=36):
         """
         type monthsToSave: int
         rtype: float
         """
-        portionSavings = 0.0005                                     
+        portionSavings = 0.0005
         # portionSavings starts with 0.0005, because the question said that the
         # accuracy is allowed to 3 digits after decimal points
         month = 0
-        while month != monthsToSave:                                # keep trying until the number of month has been tried equal to parameter: monthsToSave
-            month = super().simulate(portionSavings)                # super(): use the function that defined in mother's class
-            portionSavings += 0.0005                                # increase the portionSavings by 0.0005
+        while (
+            month != monthsToSave
+        ):  # keep trying until the number of month has been tried equal to parameter: monthsToSave
+            month = super().simulate(
+                portionSavings
+            )  # super(): use the function that defined in mother's class
+            portionSavings += 0.0005  # increase the portionSavings by 0.0005
         return portionSavings
+
 
 annualSalary = int(input("annualSalary?"))
 totalCost = int(input("totalCost?"))
 
-#publish object
-search = SearchSavingsRate(annualSalary = annualSalary, totalCost = totalCost)
+# publish object
+search = SearchSavingsRate(annualSalary=annualSalary, totalCost=totalCost)
 
-#Parameter
+# Parameter
 monthsToSave = 36
 
-#Simulating
+# Simulating
 search.binarySearch(monthsToSave)
 
-#Monitor
+# Monitor
 print(f"在{monthsToSave}個月存到頭期款所需的存款比率為{search.binarySearch(): 1.4f}")
